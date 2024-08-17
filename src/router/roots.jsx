d4e1c144a90186c0043761c82cfd437a7fa47fb9
Jubair <mallickblogger@gmail.com>
2024-08-18 00:19:12 +0600

    react router setup and add routes for productList & productsDetails

---
+++ src/router/roots.jsx
@@ -0,0 +1,12 @@
+import { createBrowserRouter } from 'react-router-dom'
+import ProductList from '../features/productManagement/components/ProductList'
+import ProductDetails from '../features/productManagement/components/ProductDetails'
+// import pages
+
+// create router
+const router = createBrowserRouter([
+    // routes
+    { path: '/', element:<ProductList/> },
+    { path: '/product/details:id', element:<ProductDetails/> }
+])
+export default router
