c9d2fc3a49d39b869d97d368ad3c702520c3068d
Jubair <mallickblogger@gmail.com>
2024-09-06 19:43:53 +0600

    feature(Add Dependent Dynamic Query)

--- src/features/orderManagement/hooks/useOrderDetails.js
+++ src/features/orderManagement/hooks/useOrderDetails.js
@@ -1,11 +1,15 @@
 import { useQuery } from "@tanstack/react-query"
-import fetchOrderDetails from "../api/OrderDetails"
-
+import { fetchOrderDetails, fetchCustomerDetails } from "../api/OrderDetails"
 const useOrderDetails = (id) => {
-    const { data, error, isLoading } = useQuery({
+    const { data: order, error, isLoading, isError } = useQuery({
         queryKey: ['order', id],
         queryFn: () => fetchOrderDetails(id)
     })
-  return {data}
+    const { data: customer, error: customerError, isLoading: customerIsLoading, isError: customerIsError } = useQuery({
+      queryKey: ['customer', order?.customer_id],
+      queryFn: () => fetchCustomerDetails(order?.customer_id),
+      enabled: !! order?.customer_id
+    })
+  return { order, error, isLoading, isError, customer, customerError, customerIsError, customerIsLoading }
 }
 export default useOrderDetails
